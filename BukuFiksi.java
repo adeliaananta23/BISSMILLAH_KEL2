dd477ec027ef92c29d390be14969567b70aa982d
Uti2609 <justiciauti@gmail.com>
2024-12-20 12:54:48 +0700

    Update BukuFiksi.java

--- BukuFiksi.java
+++ BukuFiksi.java
@@ -1,1 +1,5 @@
-
+public class BukuFiksi extends Buku {
+    public BukuFiksi(String judul, String idItem, String pengarang, String ISBN) {
+        super(judul, idItem, pengarang, ISBN, "Fiksi");
+    }
+}
