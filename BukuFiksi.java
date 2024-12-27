9e48e02081be799d864da2eec1e98cb9fa7f329b
Uti2609 <justiciauti@gmail.com>
2024-12-27 10:01:17 +0700

    Delete BukuFiksi.java

--- BukuFiksi.java
+++
@@ -1,6 +0,0 @@
-public class BukuFiksi extends Buku {
-    public BukuFiksi(String judul, String idItem, String pengarang, String ISBN) {
-        super(judul, idItem, pengarang, ISBN, "Fiksi");
-    }
-}
-
