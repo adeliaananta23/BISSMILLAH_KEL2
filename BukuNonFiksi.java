bb25866f77f6673e98252871e363837dad47982a
Uti2609 <justiciauti@gmail.com>
2024-12-27 10:01:57 +0700

    Delete BukuNonFiksi.java

--- BukuNonFiksi.java
+++
@@ -1,5 +0,0 @@
-public class BukuNonFiksi extends Buku {
-    public BukuNonFiksi(String judul, String idItem, String pengarang, String ISBN) {
-        super(judul, idItem, pengarang, ISBN, "Non-Fiksi");
-    }
-}
