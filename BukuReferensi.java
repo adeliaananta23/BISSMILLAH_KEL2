31db056c120ed568d785d82f4bf31ac43979065d
Uti2609 <justiciauti@gmail.com>
2024-12-27 10:02:31 +0700

    Delete BukuReferensi.java

--- BukuReferensi.java
+++
@@ -1,5 +0,0 @@
-public class BukuReferensi extends Buku {
-    public BukuReferensi(String judul, String idItem, String pengarang, String ISBN) {
-        super(judul, idItem, pengarang, ISBN, "Referensi");
-    }
-}
