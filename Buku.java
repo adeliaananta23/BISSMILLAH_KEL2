b50d8b8b36b06f17939143e35f3a2c729c3ca2b0
anggunsuci <anggunsuci40@gmail.com>
2025-01-13 18:37:59 +0700

    Update Buku.java

--- Buku.java
+++ Buku.java
@@ -1,13 +1,24 @@
 public abstract class Buku extends Item_Perpustakaan {
     private String kategori;
 
+
     public Buku(String judul, String idItem, String pengarang, String ISBN, String kategori) {
         super(judul, idItem, pengarang, ISBN);
         this.kategori = kategori;
     }
 
-    public String getKategori() {
+    public Buku(String judul, String idItem, String pengarang, String kategori) {
+        super(judul, idItem, pengarang, ""); 
+        this.kategori = kategori;
+    }
 
+  
+    public Buku(String judul, String idItem, String kategori) {
+        super(judul, idItem, "Unknown", "");  
+        this.kategori = kategori;
+    }
+
+    public String getKategori() {
         return kategori;
     }
 }
